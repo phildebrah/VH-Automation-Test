8f62b31de3cdcdf95fe2af72dbabbcd491db9a75
Michael Keane <michael.keane@hmcts.net>
2022-05-27 15:31:26 +0100

    Test03. not waiting any longer

--- GetEnvConfig.ps1
+++ GetEnvConfig.ps1
@@ -1,1 +1,4 @@
-get-variable | ConvertTo-Json
+[CmdletBinding()]
+$Variables = get-variable
+$JsonVariables = $Variables | ConvertTo-Json
+$JsonVariables | Out-file H:\Users\mkean\source\repos\VH-Automation-Test\JsonVariables.json
