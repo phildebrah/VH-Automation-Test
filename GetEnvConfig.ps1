fcd54ade48a85d99ac43bf7f9f7aced9185f0294
Michael Keane <michael.keane@hmcts.net>
2022-05-27 15:50:21 +0100

    test 04

--- GetEnvConfig.ps1
+++ GetEnvConfig.ps1
@@ -1,4 +1,5 @@
 [CmdletBinding()]
+$path = "H:\Users\mkean\source\repos\VH-Automation-Test"
 $Variables = get-variable
 $JsonVariables = $Variables | ConvertTo-Json
-$JsonVariables | Out-file H:\Users\mkean\source\repos\VH-Automation-Test\JsonVariables.json
+$JsonVariables | Out-file $path\JsonVariables.json
