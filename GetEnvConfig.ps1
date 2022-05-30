ff314d88a0fd6c3025c039c7775ded2f9e1d54aa
Michael Keane <michael.keane@hmcts.net>
2022-05-30 18:50:25 +0100

    check

--- GetEnvConfig.ps1
+++ GetEnvConfig.ps1
@@ -1,8 +1,14 @@
+write-output "Env"
+write-output ""
 cd Env:
 Get-ChildItem  | write-output
-#[System.Environment]::GetEnvironmentVariables() | write-output
+write-output ""
+write-output "[System.Environment]"
+[System.Environment]::GetEnvironmentVariables() | write-output
+write-output ""
 $path = "."
-$Variable1 = $env:ClientId
+#$Variable1 = $env:ClientId
+$Variable1 = Get-ChildItem
 $JsonVariables = $Variable1 | ConvertTo-Json
 $JsonVariables | Out-file $path\JsonVariables.json
 $JsonVariables | write-output
