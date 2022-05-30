c1234a8527c42b1b9afc64567eb6007744d1e4dc
Michael Keane <michael.keane@hmcts.net>
2022-05-30 21:46:37 +0100

    json

--- GetEnvConfig.ps1
+++ GetEnvConfig.ps1
@@ -9,6 +9,7 @@ write-output ""
 $path = "."
 #$Variable1 = $env:ClientId
 $Variable1 = Get-ChildItem
-$JsonVariables = $Variable1 | ConvertTo-Json -Depth 3
+$JsonVariables = $Variable1 | ConvertTo-Json -Depth 10
+# Depth of 3 no good check 10
 $JsonVariables | Out-file $path\JsonVariables.json
 $JsonVariables | write-output
