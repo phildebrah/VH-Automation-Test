38891fdab09b154c26b4fef48ba577cb25eaf154
Michael Keane <michael.keane@hmcts.net>
2022-05-30 18:39:27 +0100

    test

--- GetEnvConfig.ps1
+++ GetEnvConfig.ps1
@@ -1,5 +1,9 @@
+cd Env:
+Get-ChildItem  | write-output
+[System.Environment]::GetEnvironmentVariables() | write-output
 $path = "."
-$Variable1 = $env:AzureAd:ClientId
+
+$Variable1 = $env:ClientId
 $JsonVariables = $Variable1 | ConvertTo-Json
 $JsonVariables | Out-file $path\JsonVariables.json
 $JsonVariables | write-output
