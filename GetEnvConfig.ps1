ef86876bfb74e5eb142b0a4a13617525fd7eb5ab
Michael Keane <michael.keane@hmcts.net>
2022-05-31 07:56:32 +0100

    will this work?

--- GetEnvConfig.ps1
+++ GetEnvConfig.ps1
@@ -4,12 +4,13 @@ cd Env:
 Get-ChildItem  | write-output
 write-output ""
 write-output "[System.Environment]"
-[System.Environment]::GetEnvironmentVariables() | ConvertTo-Json | write-output
+$JsonVariables = [System.Environment]::GetEnvironmentVariables() | ConvertTo-Json
+$JsonVariables write-output
 #write-output ""
 #$path = "."
 #$Variable1 = $env:ClientId
 #$Variable1 = Get-ChildItem
 #$JsonVariables = $Variable1 | ConvertTo-Json
 # Depth of 3 no good check 10
-#$JsonVariables | Out-file $path\JsonVariables.json
+$JsonVariables | Out-file $path\JsonVariables.json
 #$JsonVariables | write-output
