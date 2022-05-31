ba66f6a7e046d39ecfafe9d6477636016d5d761b
Michael Keane <michael.keane@hmcts.net>
2022-05-31 13:49:27 +0100

    Console get envs

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+string environment = Environment.GetEnvironmentVariable("environment", EnvironmentVariableTarget.Process);
+Console.WriteLine($"environment = {environment}");
