4c5b30e0eb4d32a8b0b49d4b5fe083c82dc2fe24
Michael Keane <michael.keane@hmcts.net>
2022-05-31 14:35:23 +0100

    try this

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections;
+
+// See https://aka.ms/new-console-template for more information
 string environment = Environment.GetEnvironmentVariable("environment", EnvironmentVariableTarget.Process);
+var a = Environment.CommandLine;
+var b = Environment.GetEnvironmentVariables();
+//var c = Environment.GetFolderPath();
+var d = Environment.ProcessId;
+var e = Environment.CurrentDirectory;
+var f = Environment.UserDomainName;
 Console.WriteLine($"environment = {environment}");
+foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+    Console.WriteLine("  {0} = {1}", de.Key, de.Value);
